58f2dcb2b454e3a18d268c3bbe374a8473825413
CF8339816 <chris_french_3dga@hotmail.com>
2025-11-02 22:53:46 -0400

    post  trigger  test ,does not yet function needs work

--- Assets/scripts/teleport.cs
+++ Assets/scripts/teleport.cs
@@ -4,15 +4,15 @@ public class teleport : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Check for the player tag
+     
         if (other.CompareTag("Player"))
         {
-            // Request a random target location from the scene's manager
+          
             Transform destination = wormholeExits.Instance.GetRandomTargetPoint();
 
             if (destination != null)
             {
-                // Teleport the player's transform to the random location
+              
                 other.transform.position = destination.position;
             }
         }
